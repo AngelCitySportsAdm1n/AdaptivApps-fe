1c20b010cabed7cd0b8c8ac4856f3617eecaf464
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-07-24 18:45:14 -0400

    displays created at date on announcements

--- src/pages/Announcement/components/Messages/Announcements.js
+++ src/pages/Announcement/components/Messages/Announcements.js
@@ -1,105 +1,113 @@
-import React, { useState, useEffect, useRef } from 'react';
-
+import React, { useState, useEffect, useRef } from "react";
+// Moment.js imports
+import moment from "moment";
 // Query / Mutations
 import { useMutation } from "react-apollo";
-import { DELETE_ANNOUNCEMENT, } from '../../queries/Announcements';
-import EditAnnouncementModal from '../Modals/EditAnnouncementModal';
+import { DELETE_ANNOUNCEMENT } from "../../queries/Announcements";
+import EditAnnouncementModal from "../Modals/EditAnnouncementModal";
 
 //Auth0 imports
 import config from "../../../../config/auth_config";
 
 // Styling imports
-import Tooltip from '@material-ui/core/Tooltip';
-import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
-import DeleteIcon from '@material-ui/icons/Delete';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import {
-  makeStyles,
-  Button
-} from "@material-ui/core";
+import Tooltip from "@material-ui/core/Tooltip";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import { makeStyles, Button } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
     flexDirection: "column",
     border: "none",
-    maxWidth: '95%',
-    marginLeft: '1%'
+    maxWidth: "95%",
+    marginLeft: "1%",
   },
   messageText: {
     marginTop: "0",
     padding: "0 2%",
-    fontSize: '1.5rem'
+    fontSize: "1.5rem",
   },
   messageHeader: {
-    marginBottom: '2%',
-    padding: '1%',
-    display: 'flex',
-    justifyContent: 'space-between'
+    marginBottom: "2%",
+    padding: "1%",
+    display: "flex",
+    justifyContent: "space-between",
   },
   sender: {
-    fontSize: '1.5rem',
-    fontWeight: 'bold',
-    marginBottom: '0'
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    marginBottom: "0",
   },
   messageBox: {
-    display: 'flex',
-    alignItems: 'center',
-    marginTop: '1.5%',
-    marginLeft: '3%',
-    width: '97%'
+    display: "flex",
+    alignItems: "center",
+    marginTop: "1.5%",
+    marginLeft: "3%",
+    width: "97%",
   },
   messageSender: {
-    backgroundColor: '#C4C4C480',
-    padding: '1% 2%',
-    fontSize: '1.5rem',
-    width: '40%',
-    borderRadius: '8px'
+    backgroundColor: "#C4C4C480",
+    padding: "1% 2%",
+    fontSize: "1.5rem",
+    width: "40%",
+    borderRadius: "8px",
   },
   userMessage: {
-    backgroundColor: '#2962ff51',
-    padding: '1% 2%',
-    fontSize: '1.5rem',
-    width: '100%',
-    borderRadius: '8px'
+    backgroundColor: "#2962ff51",
+    padding: "1% 2%",
+    fontSize: "1.5rem",
+    width: "100%",
+    borderRadius: "8px",
   },
   messageDiv: {
-    maxHeight: '90vh',
-    overflowY: 'auto',
-    overflowX: 'hidden',
-    margin: '0 8%'
+    maxHeight: "90vh",
+    overflowY: "auto",
+    overflowX: "hidden",
+    margin: "0 8%",
   },
   header: {
-    fontSize: '2rem',
-    marginLeft: '4%'
+    fontSize: "2rem",
+    marginLeft: "4%",
   },
   iconDiv: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '6%',
+    display: "flex",
+    justifyContent: "space-between",
+    width: "6%",
     marginLeft: "-100px",
   },
   editIcon: {
-    '&:hover': {
-      cursor: 'pointer',
-      color: '#2962FF'
+    "&:hover": {
+      cursor: "pointer",
+      color: "#2962FF",
     },
   },
   deleteIcon: {
-    '&:hover': {
-      cursor: 'pointer',
-      color: 'red',
-    }
+    "&:hover": {
+      cursor: "pointer",
+      color: "red",
+    },
   },
   btn: {
     backgroundColor: "none",
     background: "none",
     border: "none",
+  },
+  time: {
+    textAlign: "center",
+    fontSize: "1.6rem",
+    fontStyle: "italic"
   }
 }));
 
-export default function Announcements({ announcements, user, setUpdateChat, setDeleteChat }) {
+export default function Announcements({
+  announcements,
+  user,
+  setUpdateChat,
+  setDeleteChat,
+}) {
   const classes = useStyles();
 
   const [announcementOpen, setAnnouncementOpen] = useState(false);
@@ -107,32 +115,37 @@ export default function Announcements({ announcements, user, setUpdateChat, setD
 
   const [deleteAnnouncement] = useMutation(DELETE_ANNOUNCEMENT);
 
-  const announcementArray = announcements && announcements?.announcements?.map((announcement) => {
-    return {
-      id: announcement.id,
-      title: announcement.title,
-      message: announcement.message,
-      createdAt: announcement.createdAt,
-    }
-  });
+  const announcementArray =
+    announcements &&
+    announcements?.announcements?.map(announcement => {
+      return {
+        id: announcement.id,
+        title: announcement.title,
+        message: announcement.message,
+        createdAt: announcement.createdAt,
+      };
+    });
 
   // Sets up an auto-scroll to last announcement when new announcement received, or when an announcement is updated/deleted
-  const announcementsEndRef = useRef(null)
+  const announcementsEndRef = useRef(null);
 
   const scrollToBottom = () => {
-    announcementsEndRef.current && announcementsEndRef.current.scrollIntoView({ behavior: "smooth" });
+    announcementsEndRef.current &&
+      announcementsEndRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
-  useEffect(() => { scrollToBottom() }, [announcementArray]);
+  useEffect(() => {
+    scrollToBottom();
+  }, [announcementArray]);
 
   const handleClose = () => {
     setAnnouncementOpen(false);
   };
 
   // Delete an announcement
-  const deleteMessage = async (announcement) => {
+  const deleteMessage = async announcement => {
     await deleteAnnouncement({
-      variables: { id: announcement.id }
+      variables: { id: announcement.id },
     });
     setDeleteChat(true);
   };
@@ -140,25 +153,38 @@ export default function Announcements({ announcements, user, setUpdateChat, setD
   return (
     <div className={classes.root}>
       <div className={classes.messageDiv}>
-        {announcementArray.map((announcement) => (
+        {announcementArray.map(announcement => (
           <>
             <div key={announcement.id} className={classes.messageBox}>
               <div className={classes.userMessage}>
-                <div className={classes.messageHeader}>
-                  <p className={classes.sender}>{announcement.title}</p>
-                  {user && user[config.roleUrl].includes("Admin") ? (
-                    <div className={classes.iconDiv}>
-                      <Tooltip title="Edit Announcement">
-                        <button onClick={() => { setAnnouncementOpen(true); setAnnouncementToEdit(announcement) }} className={classes.btn} >
-                          <EditOutlinedIcon className={classes.editIcon} />
-                        </button>
-                      </Tooltip>
-                      <Tooltip title="Delete Announcement">
-                        <button onClick={() => deleteMessage(announcement)} className={classes.btn} >
-                          <DeleteIcon className={classes.deleteIcon} />
-                        </button>
-                      </Tooltip>
-                    </div>) : null}
+                <div>
+                  <p className={classes.time}>{moment(announcement.createdAt).format("MMMM Do, YYYY")}</p>
+                  <div className={classes.messageHeader}>
+                    <p className={classes.sender}>{announcement.title}</p>
+                    {user && user[config.roleUrl].includes("Admin") ? (
+                      <div className={classes.iconDiv}>
+                        <Tooltip title="Edit Announcement">
+                          <button
+                            onClick={() => {
+                              setAnnouncementOpen(true);
+                              setAnnouncementToEdit(announcement);
+                            }}
+                            className={classes.btn}
+                          >
+                            <EditOutlinedIcon className={classes.editIcon} />
+                          </button>
+                        </Tooltip>
+                        <Tooltip title="Delete Announcement">
+                          <button
+                            onClick={() => deleteMessage(announcement)}
+                            className={classes.btn}
+                          >
+                            <DeleteIcon className={classes.deleteIcon} />
+                          </button>
+                        </Tooltip>
+                      </div>
+                    ) : null}
+                  </div>
                 </div>
                 <p className={classes.messageText}>{announcement.message}</p>
                 <div ref={announcementsEndRef} />
@@ -176,10 +202,15 @@ export default function Announcements({ announcements, user, setUpdateChat, setD
           BackdropComponent={Backdrop}
           BackdropProps={{
             timeout: 500,
-          }}>
-          <EditAnnouncementModal setAnnouncementOpen={setAnnouncementOpen} announcement={announcementToEdit} setUpdateChat={setUpdateChat} />
+          }}
+        >
+          <EditAnnouncementModal
+            setAnnouncementOpen={setAnnouncementOpen}
+            announcement={announcementToEdit}
+            setUpdateChat={setUpdateChat}
+          />
         </Modal>
       </div>
     </div>
-  )
-};
+  );
+}
